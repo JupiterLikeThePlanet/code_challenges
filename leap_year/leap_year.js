5c9d0e1a5904f852fe71f9ea3908c7ff113f1b56
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-01-30 23:45:19 -0800

    Add a complete refactored, shorter solution

--- leap_year/leap_year.js
+++ leap_year/leap_year.js
@@ -9,7 +9,7 @@
 // --> 1st check use an 'or' statement to see if year is not divisible by four or evenly divided by 100 and return that it is not a leap year
 // --> 2nd make an else if statement using another 'or' operator that checks if year is evenly divisible four using modulus or if the year is evenly divided by 400
 
-// Long Way
+// Long Way #1
 // var leapYear = function(year) {
 //   var leap = false;
 //   if(year % 400 === 0){
@@ -33,45 +33,46 @@
 // };
 
 // Long way #2
+// var leapYear = function(year) {
+//   var leap = false;
+//   if(year % 4 === 0){
+//     if(year % 100 !== 0){
+//       console.log("Yes, this is a leap year!");
+//       leap = true;
+//       return leap
+//     }else if(year % 100 === 0){
+//       if(year % 400 === 0){
+//         console.log("Yes, this is a leap year!");
+//         leap = true;
+//         return leap
+//       }else{
+//         console.log("No, this is not a leap year")
+//         return leap
+//       };
+//     }
+//   }else if(year % 4 !== 0){
+//     console.log("No, this is not a leap year")
+//     return leap
+//   };
+// };
+
+// Refactored shorter solution
 var leapYear = function(year) {
   var leap = false;
-  if(year % 4 === 0){
-    if(year % 100 !== 0){
-      console.log("Yes, this is a leap year!");
-      leap = true;
-      return leap
-    }else if(year % 100 === 0){
-      if(year % 400 === 0){
-        console.log("Yes, this is a leap year!");
-        leap = true;
-        return leap
-      }else{
-        console.log("No, this is not a leap year")
-        return leap
-      };
-    }
-  }else if(year % 4 !== 0){
+  if( ((year % 4 === 0) && (year % 100 != 0)) || (year % 400 === 0)){
+    console.log("Yes, this is a leap year!");
+    leap = true;
+    return leap
+  }else if(year % 4 !== 0 || year % 100 === 0){
     console.log("No, this is not a leap year")
     return leap
   };
 };
 
 
-
-//   if(year % 400 === 0 || year % 4 === 0){
-//     console.log("Yes, this is a leap year!");
-//     leap = true;
-//     return leap
-//   }else if(year % 4 !== 0 || year % 100 === 0){
-//     console.log("No, this is not a leap year")
-//     return leap
-//   };
-// };
-
-
-//console.log(leapYear(1999))
-//console.log(leapYear(2000))
-//console.log(leapYear(1900))
+console.log(leapYear(1999))
+console.log(leapYear(2000))
+console.log(leapYear(1900))
 //console.log(leapYear(1200))
 //console.log(leapYear(1493))
 //console.log(leapYear(1776))
