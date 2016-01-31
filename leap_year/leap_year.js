e8ca15181937dc92cb6c4ced32730909fa9becdb
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-01-30 22:32:45 -0800

    Complete pseudocode

--- leap_year/leap_year.js
+++ leap_year/leap_year.js
@@ -1,4 +1,10 @@
-//Problem: Check whether a given year is a leap year or not.
+// Problem: Check whether a given year is a leap year or not.
 
-//Input: A year
+// Input: A year
 // Output: A boolean value of whether the year is a leap year or not
+
+// Pseudocode
+// Create a function that takes a year as a parameter
+// Make an conditional statement:
+// --> 1st check use an 'or' statement to see if year is not divisible by four or evenly divided by 100 and return that it is not a leap year
+// --> 2nd make an else if statement using another 'or' operator that checks if year is evenly divisible four using modulus or if the year is evenly divided by 400
