38d177e68f3db39c26ea26f72a5a3fd36a8c87db
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-02-04 12:39:59 -0800

    Add Command_Line class, debug print method & completed?

--- ruby_todo/task_starter.rb
+++ ruby_todo/task_starter.rb
@@ -1,3 +1,4 @@
+require 'pp'
 class TodoList
 
   attr_accessor :list
@@ -11,7 +12,7 @@ class TodoList
   end
 
   def list_name
-    p "List Name: " @name
+    p "List Name: " + @name
   end
 
   def print_list
@@ -22,7 +23,9 @@ class TodoList
   end
 
   def add_item(item)
-    @list << Item.new(item)
+    # @list << Item.new(item)
+    item = Item.new(item)
+    @list.push(item)
     print_list
     return @list
   end
@@ -32,14 +35,12 @@ class TodoList
     return @list
   end
 
-  def edit_item(index)
-  end
-
   def completed?(index)
     if (@item.status = 'incomplete')
       return false
     else
       return true
+    end
   end
 
   def completed(index)
@@ -56,10 +57,34 @@ class Item
   attr_reader :item
   attr_accessor :status
 
-
   def initialize(item, status="incomplete")
-    @item =  + " [ ]"
+    @item =  item + " [ ]"
     @status = status
   end
 
 end
+
+
+class Command_Line
+
+  def initialize
+    @todo = TodoList.new
+  end
+
+  def enter_commands(command = nil, input = nil)
+    case command
+      when "add"
+        @todo.add_item(input)
+      when "print"
+        @todo.print_list
+      when "delete"
+        @todo.delete_item(input.to_i)
+      when "complete"
+        @todo.complete(input.to_i)
+    end
+  end
+
+end
+
+user = Command_Line.new
+user.enter_commands(ARGV[0], ARGV[1])
